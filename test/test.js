03c462cb225bd493e808274a7e14111dfc7af229
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-07-22 13:50:24 -0500

    working test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
+var path = require('path');
 var should = require('chai').should();
-
+var Cheers = require(path.join(process.cwd(), '/cheer'))
 describe('Mocha + Chai', function() {
   it('truthyness', function () {
     true.should.be.true;
